21aa804d4e4cbe9befa5ab0e66109ebab9726e97
Hernandez Aku <43397610+hernanku@users.noreply.github.com>
2021-10-09 10:43:28 -0400

    initial jinja templates.

--- web/flask-projects/daf-news-site/app.py
+++ web/flask-projects/daf-news-site/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import feedparser
 
 app = Flask(__name__)
@@ -14,28 +15,15 @@ rss_feed = {
 
 @app.route('/')
 
-@app.route('/bbc')
-def bbc():
-    return get_news('bbc')
-
-@app.route('/cnn')
-def cnn():
-    return get_news('cnn')
-
-
-
-def get_news(publication):
+@app.route('/<publication>')
+def get_news(publication="bbc"):
     feed = feedparser.parse(rss_feed[publication])
     first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>Headlines</h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
-
+    return render_template('home.html', 
+    title=first_article.get('title'),
+    published=first_article.get('published'),
+    summary=first_article.get('summary')
+    )
 
 
 if __name__ == '__main__':
