af95108134f51b08a93f12e5a3511a56abe4ffba
Hernandez Aku <43397610+hernanku@users.noreply.github.com>
2021-10-09 10:30:13 -0400

    adding cnn and fox rss feeds
    adding cnn and fox rss feeds

--- web/flask-projects/daf-news-site/app.py
+++ web/flask-projects/daf-news-site/app.py
@@ -3,20 +3,40 @@ import feedparser
 
 app = Flask(__name__)
 
-bbc_feed = "http://feeds.bbci.co.uk/news/rss.xml"
+rss_feed = {
+    'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition.rss',
+    'fox': 'http://feeds.foxnews.com/foxnews/latest',
+    'iol': 'http://www.iol.co.za/cmlink/1.640'
+}
+
+
 
 @app.route('/')
-def get_news():
-    feed = feedparser.parse(bbc_feed)
+
+@app.route('/bbc')
+def bbc():
+    return get_news('bbc')
+
+@app.route('/cnn')
+def cnn():
+    return get_news('cnn')
+
+
+
+def get_news(publication):
+    feed = feedparser.parse(rss_feed[publication])
     first_article = feed['entries'][0]
     return """<html>
         <body>
-            <h1>BBC Headlines</h1>
+            <h1>Headlines</h1>
             <b>{0}</b> <br/>
             <i>{1}</i> <br/>
             <p>{2}</p> <br/>
         </body>
     </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
